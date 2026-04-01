2ae7bc89cb7367fddf13d71238114c297d6cf159
Davi Maltez <maltezdavid438@gmail.com>
2026-04-01 18:38:18 -0300

    Add background styles and branding to login and cadastro pages

--- logout.php
+++ logout.php
@@ -1,7 +1,18 @@
 <?php
 session_start();
+
+// limpa tudo
+$_SESSION = [];
+
+// destrói sessão
 session_destroy();
 
+// força o navegador a não guardar cache
+header("Cache-Control: no-cache, no-store, must-revalidate");
+header("Pragma: no-cache");
+header("Expires: 0");
+
+// redireciona
 header("Location: login.html");
 exit();
 ?>
